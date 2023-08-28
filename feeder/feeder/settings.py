5519ec2868ac0cce1900c92a121adc481ce5f464
9seconds <nineseconds@yandex.ru>
2023-08-28 11:19:44 +0300

    Add flake8

--- feeder/feeder/settings.py
+++ feeder/feeder/settings.py
@@ -67,4 +67,4 @@ FAKEUSERAGENT_PROVIDERS = [
     "scrapy_fake_useragent.providers.FakerProvider",
     "scrapy_fake_useragent.providers.FixedUserAgentProvider",
 ]
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/116.0"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/116.0"  # noqa
