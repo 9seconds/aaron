c77564ea9dd43d920196c414a6419c5af92bd980
9seconds <nineseconds@yandex.ru>
2023-08-28 10:29:19 +0300

    Settings

--- feeder/feeder/settings.py
+++ feeder/feeder/settings.py
@@ -1,5 +1,7 @@
-BOT_NAME = "feeder"
+import os
 
+
+BOT_NAME = "feeder"
 SPIDER_MODULES = [
     "feeder.spiders",
 ]
@@ -7,53 +9,40 @@ NEWSPIDER_MODULE = "feeder.spiders"
 
 MAX_ITEMS = 10
 
+DEFAULT_ITEM_CLASS = "feeder.items.FeedEntry"
+DEPTH_PRIORITY = -0.5
+DOWNLOAD_TIMEOUT = 30
+LOG_LEVEL = "INFO"
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "feeder (+http://www.yourdomain.com)"
+MEMUSAGE_WARNING_MB = 256
+MEMUSAGE_LIMIT_MB = 4 * MEMUSAGE_WARNING_MB
+
+TELNETCONSOLE_ENABLED = False
+
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = (
+    60 *  # minute
+    60 *  # hour
+    24    # day
+)
+HTTPCACHE_GZIP = True
+HTTPCACHE_DIR = os.getenv("FEEDER_HTTPCACHE_DIR", "httpcache")
+HTTPCACHE_POLICY = "scrapy.extensions.httpcache.RFC2616Policy"
+
+RETRY_TIMES = 5
+RETRY_PRIORITY_ADJUST = -5
 
 ROBOTSTXT_OBEY = True
+COOKIES_ENABLED = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.5
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-# }
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    "feeder.middlewares.FeederSpiderMiddleware": 543,
-# }
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "feeder.middlewares.FeederDownloaderMiddleware": 543,
-# }
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
 EXTENSIONS = {
     "scrapy.extensions.closespider.CloseSpider": 100,
 }
 CLOSESPIDER_ITEMCOUNT = 2 * MAX_ITEMS
+CLOSESPIDER_TIMEOUT_NO_ITEM = 60
 
 ITEM_PIPELINES = {
     "feeder.pipelines.FeederDefaultsPipeline": 100,
@@ -61,30 +50,11 @@ ITEM_PIPELINES = {
     "feeder.pipelines.FeederMetadataPipeline": 102,
 }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
-FEED_EXPORTERS = {"atom": "feeder.exporters.FeederAtomExporter"}
+FEED_EXPORTERS = {
+    "atom": "feeder.exporters.FeederAtomExporter",
+}
